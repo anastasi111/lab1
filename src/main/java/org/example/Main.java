3ece1ea00987fdcd0b1a60a4636ebd82b7e9f35f
anastasi111 <anastasi.st@mail.ru>
2022-10-23 23:54:12 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println();
         Scanner in = new Scanner(System.in);
-        List Spisok =new List();
-        System.out.print("Enter the number of elements:");
+        List Sp =new List();
+        System.out.print("Enter the number of  required items:");
         int n=in.nextInt();
         for(int i=0;i<n;i++){
-            Spisok.add(i+1);
+            Sp.add(i+1);
         }
-        System.out.print("List generated!Display on screen:");
-        Spisok.print();
+        System.out.print("Сompiled list:");
+        Sp.print();
         System.out.println();
-        System.out.print("Enter the element to remove:");
+        System.out.print("Enter the item you want to delete:");
         int i=in.nextInt();
-        System.out.print(Spisok.remove(i-1));
+        System.out.print(Sp.remove(i-1));
         System.out.println();
-        System.out.print("Display on screen after removal:");
-        Spisok.print();
+        System.out.print("List after deleting an item:");
+        Sp.print();
         System.out.println();
-        System.out.print("Enter the element to get:");
+        System.out.print("Enter the item you want to get:");
         i=in.nextInt();
-        System.out.print(Spisok.get(i-1));
+        System.out.print(Sp.get(i-1));
         System.out.println();
     }
 }
